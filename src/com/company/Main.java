b8e310e8ad881e0fdb2ceb3c303629d9c40187f0
IhorKov <apasniy1234@gmail.com>
2021-06-23 15:32:48 +0300

    add checkValueList

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,42 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+	    List<String> list2 = new ArrayList<>();
 
+	    list2.add("vasiliy");
+	    list2.add("anastasiya");
+	    list2.add("ivan");
+	    list2.add("vasiliy");
+	    list2.add("anna");
+	    list2.add("ivan");
+
+        List<String> checkList = checkValueList(list2);
+
+        for (String s: checkList) {
+            System.out.println(s);
+        }
+
+    }
+    public static List<String> checkValueList(List<String> list) {
+        List<String> copy = new ArrayList<>(list);
+
+        for (int i = 0; i < list.size() - 1; i++){
+            String s = list.get(i);
+
+            for (int k = i + 1; k < list.size();) {
+                if (s == list.get(k)) {
+                   list.remove(k);
+                   continue;
+                }
+                k++;
+            }
+        }
+
+        return list;
+    }
 }
